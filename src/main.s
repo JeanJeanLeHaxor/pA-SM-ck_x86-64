32e8e15fc9c6cabe0466fff9ff363be1cfb05dd4
Louis Deschanel <louis@localhost.localdomain>
2022-04-26 19:08:25 +0200

    push

--- src/main.s
+++ src/main.s
@@ -1,14 +1,39 @@
-global main
+global _start
 
-main:
-    call setup_heap
-    mov rax, 0
+section .text
+
+_start:
+    push    rbp
+    mov     rsp, rbp
+    sub     rsp, 0x8
+
+    call    setup_heap
+
+    call    clear_heap
+    mov     rax, 0
     ret
 
 setup_heap:
-    mov rax, 0xc
-    mov rdi, 0x0
-    syscall
+    mov     rax, 0xc
+    mov     rdi, 0x0
+    syscall             ; brk(NULL)
+    mov     [rbp - 0x8], rax         ; save the heap start point
 
-.data
-    payload: "SALUT"
+
+clear_heap:
+    mov rax, 0xc
+    mov rdi, [rbp - 0x8]
+    syscall
+//c
+    save_heap = brk(NULL);
+    addr = sbrk(20);
+
+    ....
+    ...addr[0] = 'c';
+    printf("%c", addr[0]);
+    ...
+
+    brk(save_heap)
+
+section .data
+    payload: db "SALUT"
