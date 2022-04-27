61c64b84a0be3debd93c8fa18b881a579798e2eb
expressitoo <jrabah@myges.fr>
2022-04-27 14:17:55 -0700

    push

--- src/main.s
+++ src/main.s
@@ -1,31 +1,74 @@
+; ----------------[  Macros  ]---------------- ;
+
+%macro      brk 2
+    mov     rax, 0xc
+    mov     rdi, %1
+    add     rdi, %2
+    syscall 
+%endmacro
+
+%macro      mprotect    3
+    mov     rax, 0xa
+    mov     rdi, %1 ; start address
+    mov     rsi, %2 ; size of mprotect
+    mov     rdx, %3 ; prot flags
+    syscall
+%endmacro
+
+; -------------------------------------------- ;
+
 global _start
 
 section .text
 
 _start:
     mov     rbp, rsp
-    sub     rsp, 0x100
 
     pop     rax                 ; get the number of arguments
     cmp     rax, 0x2            ; check how many arguments there's
     jl      throw_error_arg     ; if no args then throw the usage error message
 
     pop     rax                 ; remove argv[0]
+    pop     rdi                 ; store filename of argv[1]
 
-    pop     rdi                 ; get binary with argv[1]
-    mov     rax, 0x2
-    mov     rsi, 0x0            ; RDONLY
-    syscall
+    call    open_src            ; open source file
 
-    ;to-do error file open
+    call    setup_space         ; allocate space for source file
 
-    mov rdi, rax
-    pop rsi
+    pop     r13                 ; save argv[2] in r12
+
+    mov     rsi, r14            ; location to read
+    call    read_src            ; open destination file
+    
+    ; je me suis arreté ici (dans le programme je backup le début de la heap dans r14)
     call copy_binary
 
     jmp _exit
 
+open_src:
+    mov     rax, 0x2            ; open(rdi)
+    mov     rsi, 0x2            ; O_RDWR
+    syscall
+    mov     r12, rax            ; save file descriptor of src
+    cmp     rax, 0              ; check if file was opened
+    jl      _exit               ; jmp to _exit if error
+    ret
 
+read_src:
+    mov     rdi, r12            ; get file descriptor
+    mov     rax, 0x0            ; read syscall
+    mov     rdx, 0x8            ; size
+    syscall
+    add     rsi, 0x8
+    cmp     rax, 0x8
+    jz      read_src
+    ret
+
+setup_space:
+    brk     0x0, 0x0
+    mov     r14, rax            ; save start of the heap in r14
+    brk     r14, 0x10000        ; allocate a 0x1000 bytes page
+    ret
 
 copy_binary:
     push    rdi             ; file to copy
