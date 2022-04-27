7e74886c353de42f5f072d6a52b781ae79b32a11
Louis Deschanel <louis@localhost.localdomain>
2022-04-27 19:07:13 +0200

    push

--- src/main.s
+++ src/main.s
@@ -5,22 +5,33 @@ section .text
 _start:
     mov     rbp, rsp
 
-    pop     rax
-    cmp     rax, 0x2
-    jl      error_arg
+    pop     rax                 ; get the number of arguments
+    cmp     rax, 0x3            ; check how many arguments there's
+    jl      throw_error_arg     ; if no args then throw the usage error message
 
-    pop     rax ; remove argv[0]
+    pop     rax                 ; remove argv[0]
 
-    pop     rdi ; get binary with argv[1]
+    pop     rdi                 ; get binary with argv[1]
     mov     rax, 0x2
     syscall
 
-    
+    ;to-do error file open
+
+    mov rdi, rax
+    pop rsi
+    call copy_binary
 
     jmp _exit
 
 
-error_arg:
+
+copy_binary:
+    push    rdi
+    mov     rax,    0x2
+    mov     rdi,    rsi
+    syscall
+
+throw_error_arg:
     lea rsi, [miss_arg]
     mov rax, 0x1
     mov rdi, 0x1
@@ -33,5 +44,5 @@ _exit
     syscall
 
 section .data
-    miss_arg: db "Usage: pA-SM-ck_x86-64 binary", 0xa
+    miss_arg: db "Usage: pA-SM-ck_x86-64 binary packer", 0xa
     len_miss_arg: equ $-miss_arg
